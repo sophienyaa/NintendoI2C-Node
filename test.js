bdd49318b9ae4231db1a64239b266b91240234e7
Sophie Wheeler <me@sophi.ee>
2018-02-22 21:11:40 +0000

    test

--- test.js
+++ test.js
@@ -115,7 +115,7 @@ function writeI2CtoKeyboard(delay) {
 
     setInterval(function () {
         wire.read(6, function(err, res) {
-            if(res != null || res != undefined || (res[0] == 255 && res[1] == 255) ) {
+            if(res != null || res != undefined || (res[0] != 255 && res[1] != 255) ) {
                 console.log(res);
             }
         });
